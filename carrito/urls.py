82b9f2a228ae3d87bee47ec492f2bcec6120c1d7
Leandro-Pugliese <lean.pugliese@gmail.com>
2022-04-14 18:44:55 -0300

    Shop-V1.7-Form.Carrito

--- carrito/urls.py
+++ carrito/urls.py
@@ -6,5 +6,5 @@ app_name = "carrito"
 urlpatterns = [
     path("", views.carrito_detalle, name="carrito_detalle"),
     path("add/<int:producto_id>/", views.carrito_add, name="carrito_add"),
-    path("remove/<int:producto_id>/", views.views.carrito_remove, name="carrito_remove"),
+    path("remove/<int:producto_id>/", views.carrito_remove, name="carrito_remove"),
 ]
